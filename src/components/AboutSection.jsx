cd36cc2a900317008bc9a28ff7023d498a26e641
Krupa-lakshmana <lakshmana.k@northeastern.edu>
2026-05-06 19:29:19 -0400

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -67,7 +67,7 @@ export const AboutSection = () => {
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
               <a href="#contact" className="cosmic-button">Get In Touch</a>
               <a
-                href=" "
+                href="https://github.com/Krupa-lakshmana/Krupa-Portfolio/raw/main/KrupaLakshmana_Software_Development_Engineer.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
